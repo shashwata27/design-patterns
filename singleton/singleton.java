4f02f6db6acb4aa2377d0c6ca2793cdf7feddddf
Shashwata Saha <sahas277@gmail.com>
2024-07-16 12:31:38 +0530

    add singleton class constructor

--- singleton/singleton.java
+++ singleton/singleton.java
@@ -1,5 +1,15 @@
 package singleton;
+// creational design pattern
+// only create one object of the class in the entire runtime of the app.
+// eg: database connector objects
 
-public class singleton {
-    
+public class Singleton {
+    public static void main (String[] args){
+        DBConnector con1=DBConnector.get();
+        System.out.println("con1: "+con1.hashCode());
+
+        DBConnector con2=DBConnector.get();
+        System.out.println("con2: "+con2.hashCode());
+
+    }
 }
